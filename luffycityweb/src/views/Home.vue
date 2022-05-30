e2b7c2272af02a1dc11a4f2756cbf6c3ff653fdb
mosson <mosson@qq.com>
2022-05-30 13:24:58 +0800

    feature:axios配置

--- luffycityweb/src/views/Home.vue
+++ luffycityweb/src/views/Home.vue
@@ -9,6 +9,15 @@
 <script setup>
 import Header from '../components/Header.vue';
 import Footer from '../components/Footer.vue';
+
+// 测试CORS的跨域配置是否有问题
+import axios from 'axios';
+const http = axios.create();
+
+// 测试服务端的跨域是否配置成功
+http.get('http://api.luffy.cn:8000/home/test').then((response) => {
+  console.log(response.data);
+});
 </script>
 
 <style scoped></style>
