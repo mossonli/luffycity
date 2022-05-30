2661029e21d6d3baf924551ae07b227e1be08da1
mosson <mosson@qq.com>
2022-05-30 11:44:37 +0800

    feature:跨域配置

--- luffyapi/luffyapi/settings/dev.py
+++ luffyapi/luffyapi/settings/dev.py
@@ -36,10 +36,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     "rest_framework",
+    "corsheaders",  # cors跨域子应用
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # cors跨域的中间件，必须放在CommonMiddleware前
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -48,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'luffyapi.urls'
 
--- luffycityweb/vite.config.js
+++ luffycityweb/vite.config.js
@@ -11,4 +11,20 @@ export default defineConfig({
       resolvers: [ElementPlusResolver()],
     }),
   ],
+
+  // server: {
+  //   port: '3000',           // 客户端的运行端口，此处也可以绑定vue运行的端口，当然也可以写在pycharm下
+  //   host: 'www.luffy.cn', // 客户端的运行地址，此处也可以绑定vue运行的域名，当然也可以写在pycharm下
+  //   前端进行 跨域代理
+  // 注意针对跨域的处理，前端进行或者是后端进行，不需要两端都进行！！！
+  // proxy: {
+  //     '/api': {
+  //         // 凡是遇到 /api 路径的请求，都映射到 target 属性  /api/header/  ---> http://api.luffycity.cn:8000/header/
+  //         target: 'http://api.luffy.cn:8000/',
+  //         changeOrigin: true,
+  //         ws: true,    // 是否支持websocket跨域
+  //         rewrite: path => path.replace(/^\/api/, '')
+  //     }
+  // }
+  // }
 });
