b093115482d2084c483fc12763eb34816f12141e
mosson <mosson@qq.com>
2022-05-30 10:35:44 +0800

    feature:客户端首页展示

--- luffycityweb/src/views/Home.vue
+++ luffycityweb/src/views/Home.vue
@@ -1,7 +1,14 @@
 <template>
   <h1>首页</h1>
+  <div class="home">
+    <Header></Header>
+    <Footer></Footer>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import Header from '../components/Header.vue';
+import Footer from '../components/Footer.vue';
+</script>
 
 <style scoped></style>
