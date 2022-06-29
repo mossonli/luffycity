32f1daabd05f99ec69de4fcf83d3efc3f7f7baa2
mosson <mosson@qq.com>
2022-06-29 11:48:19 +0800

    feature:阿里云oss存储

--- luffyapi/luffyapi/settings/dev.py
+++ luffyapi/luffyapi/settings/dev.py
@@ -374,7 +374,7 @@ CKEDITOR_CONFIGS = {
 # 阿里云 OSS 存储
 OSS_BUCKET_NAME = "mosson-online"  # oss 创建的 BUCKET 名称
 OSS_ENDPOINT = "oss-cn-beijing.aliyuncs.com"  # 访问域名, 根据服务器上的实际配置修改
-OSS_ACCESS_KEY_ID = "LTAI5tQq9RizqXBLJbsWduoG"
+OSS_ACCESS_KEY_ID = "LTAI5tQq9RizqXBLJbsWduo"
 OSS_ACCESS_KEY_SECRET = "F3hLy8QEupZR6qXuBcJJTku6K52NC6"
 
 # 添加下面配置后 Django admin 后台上传的 ImageField, FileField 类型的字段都会被自动上传到 oss 的服务器中, 访问路径也会自动替换
